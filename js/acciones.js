cc544a75696c0e7ede0b17cfce312cd1827cbb2a
yezy <damcb164e39@gmail.com>
2015-11-18 12:03:44 -0600

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,21 @@
 // JavaScript Document
 $(document).ready(function(e) {
+	function consulta()
+	{
+		datos="";
+		$.ajax({
+			type: "POST",
+			url: "192.168.1.30/procesar.php",
+			data: datos
+	}).done(function(mensaje){
+		alert(mensaje);
+	}); 
+	}
 document.addEventListener("deviceready",function(){
-	
+$("#btnconsulta").on("tap",function(){
+alert("preguntando...");
+consulta();
+});
 }); 
 });
 
